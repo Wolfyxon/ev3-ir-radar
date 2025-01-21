8d5025173b707d4d660b4b3d6aaf00ab7436b9b8
Wolfyxon <wolfyxon@gmail.com>
2025-01-21 19:34:54 +0100

    Init rotation

--- brick/src/main.rs
+++ brick/src/main.rs
@@ -1,6 +1,22 @@
-use ev3dev_lang_rust::Ev3Result;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+use ev3dev_lang_rust::{motors::TachoMotor, sensors::InfraredSensor, Ev3Result};
 
 fn main() -> Ev3Result<()> {
+    println!("init");
+    let motor = TachoMotor::find().expect("Motor not found");
+    let ir = InfraredSensor::find().expect("No IR sensor found");
 
-    Ok(())
+    ir.set_mode_ir_prox().unwrap();
+
+    motor.set_position(180).unwrap();
+    motor.set_speed_sp(5).unwrap();
+
+    loop {
+        let rot = motor.get_position().unwrap();
+        
+        if rot.abs() <= 180 {
+            motor.run_to_abs_pos(Some(-rot)).unwrap();
+        }
+    }
 }
